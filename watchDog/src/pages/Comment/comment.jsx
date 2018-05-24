3b8dc214211f238af650f183bb0dd99aa31b8fa2
zhangsong <zhangsong@guanghe.tv>
2018-05-24 15:11:22 +0800

    big-calendar

--- watchDog/src/pages/Comment/comment.jsx
+++ watchDog/src/pages/Comment/comment.jsx
@@ -1,30 +1,43 @@
 import React from 'react';
 import { Button } from 'antd';
-import moment from 'moment';
+import moment, { months } from 'moment';
 import './comment.css';
-import Chart from './chart.jsx';
-import { inject, observer } from 'mobx-react';
+import BigCalendar from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+// import Chart from './chart.jsx';
+// import { inject, observer } from 'mobx-react';
 
-@inject('commentStore')
-@observer
+// @inject('commentStore')
+// @observer
+BigCalendar.momentLocalizer(moment);
 export default class Comment extends React.Component {
-    switchWeek = () => {
-        this.props.commentStore.setswitchWeek(!this.props.commentStore.switchWeek)
+    // switchWeek = () => {
+    //     this.props.commentStore.setswitchWeek(!this.props.commentStore.switchWeek)
+    // }
+    constructor(props, context) {
+        super(props, context);
     }
     render() {
         return(
             <div style={{width:'100%',height:'100%'}}>
-                {this.todayRender()}
-                <Chart />
+                {/* {this.todayRender()}
+                <Chart /> */}
+                <BigCalendar
+                    culture='en-GB'
+                    events={[]}
+                    views={['week']}
+                    defaultView= 'week'
+                    toolbar = {false}
+                />
             </div>
         )
     }
-    todayRender() {
-        return(
-            <div className="btnDate">
-                <p>今天：{moment().locale('zh-cn').format('dddd')}</p>
-                <p><Button type="primary" onClick={this.switchWeek}>{this.props.commentStore.switchWeek ? '本周' : '下周' }</Button></p>
-            </div>
-        )
-    }
+    // todayRender() {
+    //     return(
+    //         <div className="btnDate">
+    //             <p>今天：{moment().locale('zh-cn').format('dddd')}</p>
+    //             <p><Button type="primary" onClick={this.switchWeek}>{this.props.commentStore.switchWeek ? '本周' : '下周' }</Button></p>
+    //         </div>
+    //     )
+    // }
 }
